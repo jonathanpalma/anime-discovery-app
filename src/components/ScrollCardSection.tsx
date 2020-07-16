217b0ab84860eb16d0527415a9628a416efa9c3f
Jonathan Palma <tanpalma04@gmail.com>
2020-07-16 13:24:40 -0600

    feat: add card scroll section

---
+++ src/components/ScrollCardSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import Card from '@app/components/Card';
+import { Item } from '@app/ts/types';
+
+type Props = {
+  items: Item[];
+  title: string;
+};
+
+type ItemProps = {
+  item: Item;
+};
+
+function ScrollCardSection({ items = [], title }: Props) {
+  const renderItem = ({ item }: ItemProps) => (
+    <Card title={item.title} image={item.image} />
+  );
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        contentContainerStyle={styles.horizontalScrollContainer}
+        data={items}
+        horizontal={true}
+        keyExtractor={(item) => item.slug}
+        renderItem={renderItem}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+export default ScrollCardSection;
+
+const styles = StyleSheet.create({
+  horizontalScrollContainer: {
+    paddingBottom: 20,
+    paddingEnd: 20,
+  },
+  title: {
+    color: '#b8bece',
+    fontWeight: '600',
+    fontSize: 15,
+    marginLeft: 20,
+    marginTop: 20,
+    textTransform: 'uppercase',
+  },
+});
---
+++ src/ts/types.ts
@@ -0,0 +1,6 @@
+// Item can be either an Anime or Manga
+export type Item = {
+  slug: string;
+  title: string;
+  image: string;
+};
