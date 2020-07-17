78dca0b15ac148a994d5d31d8093d8c983d18c10
Jonathan Palma <tanpalma04@gmail.com>
2020-07-17 12:23:34 -0600

    feat: add detail navigation params

--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from 'react-navigation-hooks';
 
 function Detail() {
+  const { getParam } = useNavigation();
+  const image = getParam('image', '');
+  const title = getParam('title', '');
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Text>Detail: {title}</Text>
+        {image !== '' && <Image style={styles.image} source={{ uri: image }} />}
+      </SafeAreaView>
     </View>
   );
 }
 
 export default Detail;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: 100,
+    width: 100,
+    // position: 'absolute',
+    // left: 0,
+    // top: 0,
+  },
+});
