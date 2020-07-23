ef202789435cdf0d29342434dce55d88c7efff25
Jonathan Palma <tanpalma04@gmail.com>
2020-07-22 21:28:52 -0600

    feat: add github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   COLOR_BLACK,
   COLOR_FIORD,
   COLOR_GRAY_HEATHER,
 } from '@app/constants/colors';
 
+const openGithub = () => {
+  Linking.openURL('https://github.com/jonathanpalma');
+};
+
 function Header() {
   return (
     <View style={styles.header}>
-      <Image style={styles.avatar} source={require('../assets/avatar.jpeg')} />
       <View style={styles.container}>
-        <Text style={styles.title}>Anime Discovery by:</Text>
-        <Text style={styles.name}>Jonathan Palma</Text>
+        <Image
+          style={styles.avatar}
+          source={require('../assets/avatar.jpeg')}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Anime Discovery by:</Text>
+          <Text style={styles.name}>Jonathan Palma</Text>
+        </View>
       </View>
+      <TouchableNativeFeedback useForeground={true} onPress={openGithub}>
+        <View style={styles.touchable}>
+          <Icon name="github" size={32} color={COLOR_FIORD} />
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
@@ -22,8 +44,9 @@ const styles = StyleSheet.create({
   header: {
     width: '100%',
     marginVertical: 20,
-    paddingLeft: 20,
+    paddingHorizontal: 20,
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   avatar: {
@@ -33,6 +56,10 @@ const styles = StyleSheet.create({
     borderRadius: 22,
   },
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textContainer: {
     paddingLeft: 10,
   },
   title: {
@@ -44,6 +71,19 @@ const styles = StyleSheet.create({
     color: COLOR_FIORD,
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  touchable: {
+    paddingHorizontal: 13,
+    paddingVertical: 10,
+    marginHorizontal: -13,
+    marginVertical: -10,
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  icon: {
+    position: 'absolute',
+    right: 20,
+    top: 5,
   },
 });
 
