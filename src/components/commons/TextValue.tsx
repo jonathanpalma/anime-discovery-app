5ac9da4ff9c0025330993a987f16cf7b5b574d81
Jonathan Palma <tanpalma04@gmail.com>
2020-07-27 13:11:46 -0600

    feat: add text view more/less

--- src/components/commons/TextValue.tsx
+++ src/components/commons/TextValue.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { COLOR_FIORD } from '@app/constants/colors';
+import ReadMore from 'react-native-read-more-text';
+import { COLOR_FIORD, COLOR_GRAY_HEATHER } from '@app/constants/colors';
 
 type Props = {
   label: string;
   value?: string;
 };
 
+const renderTruncatedFooter = (handlePress: () => void) => (
+  <Text style={styles.textToggle} onPress={handlePress}>
+    View more
+  </Text>
+);
+
+const renderRevealedFooter = (handlePress: () => void) => (
+  <Text style={styles.textToggle} onPress={handlePress}>
+    View less
+  </Text>
+);
+
 function TextValue({ label, value }: Props) {
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <Text style={styles.value}>{value}</Text>
+      <ReadMore
+        numberOfLines={4}
+        renderTruncatedFooter={renderTruncatedFooter}
+        renderRevealedFooter={renderRevealedFooter}
+      >
+        <Text style={styles.value}>{value}</Text>
+      </ReadMore>
     </View>
   );
 }
@@ -26,6 +45,11 @@ const styles = StyleSheet.create({
     color: COLOR_FIORD,
     fontSize: 14,
     fontWeight: '500',
+  },
+  textToggle: {
+    color: COLOR_GRAY_HEATHER,
+    marginTop: 5,
+    marginBottom: 10,
   },
 });
 
